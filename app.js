e5355bccd62de6206de2bcafa1565935b3c9f28c
Minji Ma <gnkryd12@gmail.com>
2020-06-15 10:40:57 +0900

    Delete non-necessary code

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ var StyleExcelRouter = require('./routes/StyleExcel/style');
 var GetFeedRouter = require('./routes/GetFeed/feed');
 
 var app = express();
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', indexRouter);
@@ -15,11 +20,11 @@ app.use('/styleexcel', StyleExcelRouter);
 app.use('/getfeed', GetFeedRouter);
 
 app.set('views', __dirname + '/views');
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.engine('html', require('ejs').renderFile);
 
-const server = app.listen(3000,function(){
-  server.setTimeout( 30 * 60 * 1000 );
+
+app.listen(3000, function () {
   console.log('Connected 3000 port!')
 });
